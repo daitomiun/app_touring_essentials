b069b8a8b67ffd949c7daf853d45d160d708d8fa
Mateo Rodriguez <mateo.rodriguez.fierro@gmail.com>
2023-11-26 12:35:55 -0500

    errors fixed from failed build

--- src/components/utils/getObjectEntries.js
+++ src/components/utils/getObjectEntries.js
@@ -1,7 +1,9 @@
 export default function getObjectEntries(object) {
   let str = [];
+  let keys = [];
   for (const [key, value] of Object.entries(object)) {
     str.push(value);
+    keys.push(key);
   }
   return str.join(", ");
 }
